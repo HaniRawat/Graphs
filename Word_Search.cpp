a849f87f71dbbb7c9125e196b3b52e94f4581a9d
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2024-08-31 20:44:01 +0530

    solved

---
+++ Word_Search.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    int n, m;
+    
+    bool find(int i, int j, vector<vector<char>>&board, string word, int ind){
+        
+        if(ind == word.size())
+            return true;
+            
+        if(i<0 || i>=n || j<0 || j>=m || board[i][j] == '$')
+            return false;
+            
+        if(board[i][j] != word[ind])
+            return false;
+            
+        char temp = board[i][j];
+        board[i][j] = '$';
+        
+        if(find(i+1, j, board, word, ind+1) || find(i-1, j, board, word, ind+1)
+        || find(i, j+1, board, word, ind+1) || find(i, j-1, board, word, ind+1)){
+            return true;
+        }
+        
+        board[i][j] = temp;
+        return false;
+    }
+    bool isWordExist(vector<vector<char>>& board, string word) {
+        
+        n = board.size();
+        m = board[0].size();
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(board[i][j] == word[0] && find(i,j,board,word,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<char>>board(n, vector<char>(m, '*'));
+		for(int i = 0; i < n; i++)
+			for(int j = 0; j < m; j++)
+				cin >> board[i][j];
+		string word;
+		cin >> word;
+		Solution obj;
+		bool ans = obj.isWordExist(board, word);
+		if(ans)
+			cout << "1\n";
+		else cout << "0\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
